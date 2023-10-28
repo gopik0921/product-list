caffa575684024a10dff4a43ce3c0da0878ee245
gopikrishna <gopikrishnamathi@gmail.com>
2023-10-28 16:53:54 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-function App() {
+import productReducer from './redux/reducers';
+import DashBoard from './components/DashBoard';
+
+const store = createStore(productReducer);
+
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+     <DashBoard/>
+    </Provider>
   );
-}
+};
 
 export default App;
+
